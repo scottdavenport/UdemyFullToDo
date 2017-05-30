f2184c59496e2d736b4e08bbd28566fc577e8167
TarSoxJr <tarsox5@gmail.com>
2017-05-30 11:58:22 -0400

    Initial Commit (Pre-styling)

---
+++ assets/js/todos.js
@@ -0,0 +1,22 @@
+//Check off specific todos by clicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+//click on x to delete to-do
+$("ul").on("click", "span", function(event){
+  event.stopPropagation();
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13) {
+    //grabbing new text from input
+    var todoText = $(this).val();
+    //create a new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    $(this).val("");
+  }
+});
